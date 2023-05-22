7c9726eb00c68266471f33842c011b6ced8f1f40
Scott Motte <mot@mot.la>
2023-05-22 15:52:31 -0700

    Adds .env.vault

--- pages/api/location.js
+++ pages/api/location.js
@@ -3,5 +3,5 @@ export const config = {
 };
 
 export default function handler(req) {
-  return Response.json({ location: req.headers.get('x-vercel-ip-city') || 'world' });
+  return Response.json({ location: process.env.HELLO });
 }
